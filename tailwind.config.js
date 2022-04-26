986a5c6dfe5ec462f234f2e0a79fad85a5f63c16
natainditama <natainditama@gmail.com>
2022-04-26 09:48:26 +0700

    Add feat/form: Complete create form components

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Nunito", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
---
+++ webpack.mix.js
@@ -0,0 +1,23 @@
+const mix = require("laravel-mix");
+
+mix.browserSync({
+    proxy: "http://127.0.0.1:8000",
+    port: 80,
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js("resources/js/app.js", "public/js").postCss(
+    "resources/css/app.css",
+    "public/css",
+    [require("tailwindcss")]
+);
