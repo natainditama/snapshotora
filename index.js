9d3343c64f1a7d613b93ac48ca3e5acb68a7ae31
Nata Inditama <natainditama.dev@gmail.com>
2024-01-22 20:58:36 +0800

    chore: add url config

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const { chromium } = require("playwright");
+const core = require("@actions/core");
+
+const filename = "screenshot.png";
+const workspace = process.env.GITHUB_WORKSPACE;
+const path = workspace ? `${workspace}/` : `./`;
+const filePath = `${path}/${filename}`;
 
 (async () => {
   try {
+    const url = core.getInput("url", { required: true });
+
     const browser = await chromium.launch();
     const page = await browser.newPage();
-    await page.goto("https://github.com/natainditama");
-    await page.screenshot({ path: "screenshot.png" });
+    await page.goto(url);
+    await page.screenshot({ path: filePath });
 
     await browser.close();
   } catch (error) {
     console.log(error);
+    core.setFailed(error.message);
   }
 })();
