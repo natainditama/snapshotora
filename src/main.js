fb18aeadd341eed33b652afb6ce0e1afd2453862
Nata Inditama <natainditama.dev@gmail.com>
2024-01-26 22:55:23 +0800

    chore: implement args

---
+++ src/main.js
@@ -0,0 +1,68 @@
+const core = require("@actions/core");
+const { isValidURL, wait } = require("./utils.js");
+const { chromium } = require("playwright");
+
+const DEFAULT_TYPE = "png";
+const DEFAULT_FILENAME = "screenshot.jpg";
+
+async function run() {
+  try {
+    let delay = core.getInput("delay") || "0";
+    const url = core.getInput("url", {}) || "ada";
+    const fullPage = core.getInput("fullPage") === "true";
+    let filename = core.getInput("filename") || DEFAULT_FILENAME;
+    let screenshotType = core.getInput("type") || DEFAULT_TYPE;
+
+    if (!isValidURL(url)) {
+      core.error("url must be a valid URL");
+      return;
+    }
+
+    delay = parseInt(delay);
+    if (Number.isNaN(delay)) {
+      core.debug("delay must be a number");
+      delay = 0;
+    }
+
+    filename = filename.includes(".")
+      ? filename.slice(0, filename.lastIndexOf("."))
+      : filename;
+
+    const includedTypes = ["png", "jpeg"];
+    screenshotType = screenshotType.toLowerCase();
+    if (!includedTypes.includes(screenshotType)) {
+      core.debug(`type must be one of [${includedTypes.join(", ")}]`);
+      screenshotType = DEFAULT_TYPE;
+    }
+
+    core.startGroup("inputs");
+    console.log("inputs: ", {
+      url,
+      fullPage,
+      filename,
+      type: screenshotType,
+    });
+    core.endGroup();
+
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await wait(delay);
+
+    const path = process.env.GITHUB_WORKSPACE || ".";
+    const filePath = `${path}/${filename}.${screenshotType}`;
+    await page.screenshot({
+      path: filePath,
+      fullPage,
+    });
+    await browser.close();
+
+    core.setOutput("filename", filePath);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = {
+  run,
+};
