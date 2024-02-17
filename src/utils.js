37b5a8f522d0e917ed8be1dc23780eef2fb5088f
Nata Inditama <natainditama.dev@gmail.com>
2024-02-17 23:26:38 +0800

    chore: add repack

--- src/utils.js
+++ src/utils.js
@@ -13,7 +13,18 @@ function isValidURL(url) {
   );
 }
 
+async function getExecutablePath() {
+  const executables = {
+    Linux: "/usr/bin/google-chrome",
+    Windows: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    macOS: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  };
+
+  return executables[process.env.RUNNER_OS];
+}
+
 module.exports = {
+  getExecutablePath,
   isValidURL,
   wait,
 };
